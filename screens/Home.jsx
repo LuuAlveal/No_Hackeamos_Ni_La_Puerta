b8103bb859110c71a613189eae56579753aaffab
NaZYTa_149 <facundo.alvarez3247@gmail.com>
2024-08-20 16:48:07 -0300

    Asignamos el nombre Ajustes para el boton

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -62,6 +62,13 @@ export default function Home (){
                     </text>
                     </TouchableOpacity>
                 </View> 
+                <View style = {style.containerButton}>
+                    <TouchableOpacity style = {style.button}>
+                        <text style={style.textButton} >
+                            Ajustes
+                        </text>
+                        </TouchableOpacity>
+                </View>
                 <Image                       
                     source={require('../assets/3b4c0036b590cc1977d03230bb7d34f0.jpg')}
                     style = {{
