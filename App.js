e45309e00b365b7c9cde105cd772d09b17a22a59
Marcos-Ortega <marcosortega977@gmail.com>
2024-09-15 00:47:46 -0300

    Organizando Stack

--- App.js
+++ App.js
@@ -1,74 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
 import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './screens/Login';
-import Home from './screens/Home';
-import Register from './screens/Register';
-import Opciones from './screens/Opciones';
+import MyStack from './Stack/MyStack';
+
 export default function App() {
-  const Stack = createStackNavigator();
 
-  function MyStack() {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Login" 
-          component={ Login } 
-          options={{
-            title: "E.P.E.T N 20",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { 
-              backgroundColor: '#304A6E'
-            }
-          }}
-      />
-
-      <Stack.Screen 
-          name="Register" 
-          component={ Register } 
-          options={{
-            title: "E.P.E.T N 20",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: '#304A6E',
-            }
-          }}
-      />
-
-      <Stack.Screen 
-          name="Home" 
-          component={ Home } 
-          options={{
-            title: "E.P.E.T N 20",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: '#304A6E'
-            }
-          }}
-      />
-        <Stack.Screen 
-          name="Opciones" 
-          component={ Opciones } 
-          options={{
-            title: "E.P.E.T N 20",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: '#304A6E'
-            }
-          }}
-      />
-
-      </Stack.Navigator>
-  );
-
-}
   return (
     <NavigationContainer>
       <MyStack/>
---
+++ Stack/MyStack.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from '../screens/Login';
+import Home from '../screens/Home';
+import Register from '../screens/Register';
+import Opciones from '../screens/Opciones';
+
+const Stack = createStackNavigator();
+  
+export default function MyStack() {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="Login" 
+            component={ Login } 
+            options={{
+              title: "E.P.E.T N 20",
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerStyle: { 
+                backgroundColor: '#304A6E'
+              }
+            }}
+        />
+  
+        <Stack.Screen 
+            name="Register" 
+            component={ Register } 
+            options={{
+              title: "E.P.E.T N 20",
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: '#304A6E',
+              }
+            }}
+        />
+  
+        <Stack.Screen 
+            name="Home" 
+            component={ Home } 
+            options={{
+              title: "E.P.E.T N 20",
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: '#304A6E'
+              }
+            }}
+        />
+          <Stack.Screen 
+            name="Opciones" 
+            component={ Opciones } 
+            options={{
+              title: "E.P.E.T N 20",
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: '#304A6E'
+              }
+            }}
+        />
+  
+        </Stack.Navigator>
+    );
+  
+}
