8539147ec51bd71212a183ec26957cea5fd43015
LuuAlveal <luanalveal2005@gmail.com>
2024-09-15 23:35:01 -0300

    Agrego botones en ayuda

--- screens/ayuda.jsx
+++ screens/ayuda.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default function Ayuda () {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity>
-                    <Text>
-                        PRUEBA1
-                    </Text>    
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
-
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#222',
+      backgroundColor: '#304A6E',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -30,3 +16,37 @@ const styles = StyleSheet.create({
       padding: 20,
     },
 });
+
+export default function Ayuda () {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Text>
+                        ¿Cómo puedo anotarme a una materia?
+                    </Text>    
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                        ¿Cómo puedo registrarme?
+                    </Text>    
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                    ¿Cómo puedo saber los datos de la materia a la que me quiero anotar?
+                    </Text>    
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                    ¿Cómo puedo contactar a la escuela?
+                    </Text>    
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                    ¿Hasta en cuantas materias me puedo anotar?
+                    </Text>    
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
