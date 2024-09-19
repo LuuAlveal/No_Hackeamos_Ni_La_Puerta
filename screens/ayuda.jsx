2024055b56529ada65335f5edbebec49db82ffee
LuuAlveal <luanalveal2005@gmail.com>
2024-09-19 00:16:34 -0300

    Arreglando links

--- screens/ayuda.jsx
+++ screens/ayuda.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { Linking } from 'react-native';
 
-const handlePress = () => {
+const AnotarseMateria = () => {
     Linking.openURL('https://drive.google.com/file/d/1mGvps6PheUVDiOZqAsK6akfrO_bBrEqZ/view?usp=drive_link');
+};
+const Registrase = () => {
+    Linking.openURL('https://drive.google.com/file/d/1xLjtTgbe85Apy_AMc0hN06OFY8yGz9sk/view?usp=drive_link');
+};
+const DatosMateria = () => {
+    Linking.openURL('https://drive.google.com/file/d/1mGvps6PheUVDiOZqAsK6akfrO_bBrEqZ/view?usp=drive_link');
+};
+const InformacionEscuela = () => {
+    Linking.openURL('https://drive.google.com/file/d/1uRFxjUZjo2O91N5xR6gSk_RUUh4coeSP/view?usp=drive_link');
+};
+const MaximoMaterias = () => {
+    Linking.openURL('https://drive.google.com/file/d/1YLziNl5WJ8bm7OO64sU6lwXUX_GZfWYl/view?usp=drive_link');
 };
 const styles = StyleSheet.create({
     container: {
@@ -30,7 +42,7 @@ export default function Ayuda () {
             <View style={styles.header}>
                 <TouchableOpacity 
                     style={styles.optionButton}
-                    onPress={handlePress}
+                    onPress={AnotarseMateria}
                     >
                     <Text>
                         ¿Cómo puedo anotarme a una materia?
@@ -38,7 +50,7 @@ export default function Ayuda () {
                 </TouchableOpacity>
                 <TouchableOpacity 
                     style={styles.optionButton}
-                    onPress={handlePress}
+                    onPress={Registrase}
                     >
                     <Text>
                         ¿Cómo puedo registrarme?
@@ -46,7 +58,7 @@ export default function Ayuda () {
                 </TouchableOpacity>
                 <TouchableOpacity 
                     style={styles.optionButton}
-                    onPress={handlePress}
+                    onPress={DatosMateria}
                     >
                     <Text>
                     ¿Cómo puedo saber los datos de la materia a la que me quiero anotar?
@@ -54,7 +66,7 @@ export default function Ayuda () {
                 </TouchableOpacity>
                 <TouchableOpacity 
                     style={styles.optionButton}
-                    onPress={handlePress}
+                    onPress={InformacionEscuela}
                     >
                     <Text>
                     ¿Cómo puedo contactar a la escuela?
@@ -62,7 +74,7 @@ export default function Ayuda () {
                 </TouchableOpacity>
                 <TouchableOpacity 
                     style={styles.optionButton}
-                    onPress={handlePress}
+                    onPress={MaximoMaterias}
                     >
                     <Text>
                     ¿Hasta en cuantas materias me puedo anotar?
