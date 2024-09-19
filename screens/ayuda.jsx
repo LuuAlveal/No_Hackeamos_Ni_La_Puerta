5be923c5473aaf3e641649c1d86654bed1a08e21
LuuAlveal <luanalveal2005@gmail.com>
2024-09-18 23:55:08 -0300

    Agregando estilo en ayuda

--- screens/ayuda.jsx
+++ screens/ayuda.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#304A6E',
+    },
+    optionButton: {
+      padding: 16,
+      textAlign: 'center',
+      borderRadius: 12,
+      marginVertical: 12,
+      alignItems: 'center',
+      width: '80%',
+      height: 60,
+      backgroundColor: '#FFFAFA'
+    }
+  });
+
 export default function Ayuda () {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
                     <Text>
                         ¿Cómo puedo anotarme a una materia?
                     </Text>    
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
                     <Text>
                         ¿Cómo puedo registrarme?
                     </Text>    
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
                     <Text>
                     ¿Cómo puedo saber los datos de la materia a la que me quiero anotar?
                     </Text>    
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
                     <Text>
                     ¿Cómo puedo contactar a la escuela?
                     </Text>    
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
                     <Text>
                     ¿Hasta en cuantas materias me puedo anotar?
                     </Text>    
@@ -33,19 +52,4 @@ export default function Ayuda () {
             </View>
         </View>
     )
-}
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#304A6E',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    header: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      padding: 20,
-    },
-});
+};
