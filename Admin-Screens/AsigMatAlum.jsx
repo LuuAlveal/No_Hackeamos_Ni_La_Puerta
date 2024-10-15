b47d89f93571574d414d3247d096763308ab336b
Pichiman <facundopichiman@gmail.com>
2024-10-14 23:19:39 -0300

    Ordenando codigo de AsigMatAlum

--- Admin-Screens/AsigMatAlum.jsx
+++ Admin-Screens/AsigMatAlum.jsx
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 const BD = getFirestore(appFirebase);
 
 export default function AsigMatAlum(props) {
- /*   const [alumno, setAlumno] = useState({ nombre: '', apellido: '', dni: '',year:'' });
+/*   const [alumno, setAlumno] = useState({ nombre: '', apellido: '', dni: '',year:'' });
     const [selectedYear, setSelectedYear] = useState("default");
     //Leer el document del alumno
     const getAlumnoById = async (id) => {
@@ -50,12 +50,12 @@ export default function AsigMatAlum(props) {
             })
             console.log(error)
         }
-    };
-*/
-const [selectedYear, setSelectedYear] = useState("default");
-const [selectedSubject, setSelectedSubject] = useState("default");
-const [numberOfSubjects, setNumberOfSubjects] = useState("default");
-const materiasPorAño = {
+    };*/
+
+    const [selectedYear, setSelectedYear] = useState("default");
+    const [selectedSubject, setSelectedSubject] = useState("default");
+    const [numberOfSubjects, setNumberOfSubjects] = useState("default");
+    const materiasPorAño = {
     '1°': [
         { label: 'Biología', value: 'Biología' },
         { label: 'Construccion de la Ciudadanía', value: 'Construccion de la Ciudadanía' },
@@ -148,153 +148,137 @@ const materiasPorAño = {
         { label: 'Taller', value: 'Taller' },
         { label: 'Tecnología de Redes II', value: 'Tecnología de Redes II' }
     ]
-};
-
-
-    const style = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        form: {
-            margin: 20,
-            padding: 40,
-            backgroundColor: 'rgba(255, 255, 255, 0.8)',
-            borderRadius: 10,
-            shadowColor: '#000',
-            shadowOffset: {
-                width: 10,
-                height: 10,
-            },
-            shadowOpacity: 0.5,
-            shadowRadius: 6,
-            elevation: 5,
-        },
-        backgroundImage: {
-            flex: 1,
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        modificarAlumno: {
-            fontSize: 18,
-            fontFamily: 'sans-serif',
-            textAlign: 'center'
-        },
-        cajaIng: {
-            paddingVertical: 10,
-            backgroundColor: 'white',
-            borderRadius: 30,
-            marginBottom: 10,
-            marginTop: 2,
-            borderColor: 'white',
-        },
-        containerButton: {
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '100%'
-        },
-        button: {
-            backgroundColor: '#4F76AC',
-            borderRadius: 30,
-            paddingVertical: 10,
-            marginTop: 20,
-            width: 150,
-            marginHorizontal: 10
-        },
-        textButton: {
-            textAlign: 'center',
-            color: 'white',
-            fontFamily: 'sans-serif'
-        }
-    });
-    const asignarMateria = () => {
-        if (numberOfSubjects === "default" || selectedYear === "default" || selectedSubject === "default") {
-            alert("Por favor, seleccione la cantidad de materias, el año y la materia.");
-            return;
-        }
-    
-        // Aquí iría la lógica para asignar la materia al alumno
-        console.log(`Asignando ${numberOfSubjects} materia(s): ${selectedSubject} de ${selectedYear} año`);
-    
-        // Aquí podrías agregar la lógica para guardar esta información en tu base de datos
-        // Por ejemplo, usando Firebase/Firestore
-    
-        // Después de asignar, podrías resetear los estados
-        setNumberOfSubjects("default");
-        setSelectedYear("default");
-        setSelectedSubject("default");
-    
-        // Y mostrar un mensaje de éxito
-        alert("Materia(s) asignada(s) con éxito");
     };
-    return (
-        
+    const asignarMateria = () => {
+    if (numberOfSubjects === "default" || selectedYear === "default" || selectedSubject === "default") {
+        alert("Por favor, seleccione la cantidad de materias, el año y la materia.");
+        return;
+    }
+        // Aquí iría la lógica para asignar la materia al alumno
+    console.log(`Asignando ${numberOfSubjects} materia(s): ${selectedSubject} de ${selectedYear} año`);    
+        // Aquí podrías agregar la lógica para guardar esta información en tu base de datos
+        // Por ejemplo, usando Firebase/Firestore    
+        // Después de asignar, podrías resetear los estados
+    setNumberOfSubjects("default");
+    setSelectedYear("default");
+    setSelectedSubject("default");
+        // Y mostrar un mensaje de éxito
+    alert("Materia(s) asignada(s) con éxito");
+    };
+    const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    form: {
+        margin: 20,
+        padding: 40,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 10,
+            height: 10,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+        elevation: 5,
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modificarAlumno: {
+        fontSize: 18,
+        fontFamily: 'sans-serif',
+        textAlign: 'center'
+    },
+    cajaIng: {
+        paddingVertical: 10,
+        backgroundColor: 'white',
+        borderRadius: 30,
+        marginBottom: 10,
+        marginTop: 2,
+        borderColor: 'white',
+    },
+    containerButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    button: {
+        backgroundColor: '#4F76AC',
+        borderRadius: 30,
+        paddingVertical: 10,
+        marginTop: 20,
+        width: 150,
+        marginHorizontal: 10
+    },
+    textButton: {
+        textAlign: 'center',
+        color: 'white',
+        fontFamily: 'sans-serif'
+    }
+    });
 
+    return (        
         <ImageBackground
             source={require('../assets/epet20fondo.png')}
             resizeMode={'cover'}
-            style={style.backgroundImage}
-        >
-                
+            style={style.backgroundImage}>                
+            <View style={style.form}>
+                <Text style={style.modificarAlumno}>Asignar MATERIAS</Text>
 
-                <View style={style.form}>
-    <Text style={style.modificarAlumno}>Asignar MATERIAS</Text>
+                <Text style={{ fontSize: 15 }}> Cuantas materias va a rendir</Text>
+                <Picker
+                    selectedValue={numberOfSubjects}
+                    onValueChange={(itemValue) => setNumberOfSubjects(itemValue)}
+                    style={style.cajaIng}>
+                    <Picker.Item label="Seleccione las materias" value="default" />
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                </Picker>
 
-    <Text style={{ fontSize: 15 }}> Cuantas materias va a rendir</Text>
-    <Picker
-        selectedValue={numberOfSubjects}
-        onValueChange={(itemValue) => setNumberOfSubjects(itemValue)}
-        style={style.cajaIng}
-    >
-        <Picker.Item label="Seleccione las materias" value="default" />
-        <Picker.Item label="1" value="1" />
-        <Picker.Item label="2" value="2" />
-        <Picker.Item label="3" value="3" />
-    </Picker>
-
-    <Text style={{ fontSize: 15 }}>Año</Text>
-    <Picker 
-        selectedValue={selectedYear}
-        onValueChange={(itemValue) => {
-            setSelectedYear(itemValue);
-            setSelectedSubject("default");
-        }}
-        style={style.cajaIng}
-    >
-        <Picker.Item label="Seleccione el año de la materia" value="default"/>
-        <Picker.Item label="1° año" value="1°" />
-        <Picker.Item label="2° año" value="2°" />
-        <Picker.Item label="3° año" value="3°" />
-        <Picker.Item label="4° año" value="4°" />
-        <Picker.Item label="5° año" value="5°" />
-        <Picker.Item label="6° año" value="6°" />
-    </Picker>
-
-    {selectedYear !== "default" && (
-        <>
-            <Text style={{ fontSize: 15 }}>Materia</Text>
-            <Picker
-                selectedValue={selectedSubject}
-                onValueChange={(itemValue) => setSelectedSubject(itemValue)}
-                style={style.cajaIng}
-            >
-                <Picker.Item label="Seleccione la materia" value="default" />
-                {materiasPorAño[selectedYear].map((materia) => (
-                    <Picker.Item key={materia.value} label={materia.label} value={materia.value} />
-                ))}
-            </Picker>
-        </>
-    )}
-
-    <View style={style.containerButton}>
-        <TouchableOpacity style={style.button} onPress={asignarMateria}>
-            <Text style={style.textButton}>Asignar Materia</Text>
-        </TouchableOpacity>
-    </View>
-</View>
+                <Text style={{ fontSize: 15 }}>Año</Text>
+                <Picker 
+                    selectedValue={selectedYear}
+                    onValueChange={(itemValue) => {
+                        setSelectedYear(itemValue);
+                        setSelectedSubject("default");
+                    }}
+                    style={style.cajaIng}>
+                    <Picker.Item label="Seleccione el año de la materia" value="default"/>
+                    <Picker.Item label="1° año" value="1°" />
+                    <Picker.Item label="2° año" value="2°" />
+                    <Picker.Item label="3° año" value="3°" />
+                    <Picker.Item label="4° año" value="4°" />
+                    <Picker.Item label="5° año" value="5°" />
+                    <Picker.Item label="6° año" value="6°" />
+                </Picker>
+                {selectedYear !== "default" && (
+                    <>
+                        <Text style={{ fontSize: 15 }}>Materia</Text>
+                        <Picker
+                            selectedValue={selectedSubject}
+                            onValueChange={(itemValue) => setSelectedSubject(itemValue)}
+                            style={style.cajaIng}>
+                            <Picker.Item label="Seleccione la materia" value="default" />
+                            {materiasPorAño[selectedYear].map((materia) => (
+                                <Picker.Item key={materia.value} label={materia.label} value={materia.value} />
+                            ))}
+                        </Picker>
+                    </>
+                )}
+                <View style={style.containerButton}>
+                    <TouchableOpacity style={style.button} onPress={asignarMateria}>
+                        <Text style={style.textButton}>Asignar Materia</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </ImageBackground>
-    );
-    
+    );    
 }
