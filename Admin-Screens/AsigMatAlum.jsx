8f16cd980845fbfb5df84706787756e808d0d4f6
Marcos-Ortega <marcosortega977@gmail.com>
2024-11-10 14:07:48 -0300

    Limpiado un poco el codigo

--- Admin-Screens/AsigMatAlum.jsx
+++ Admin-Screens/AsigMatAlum.jsx
@@ -17,106 +17,7 @@ export default function AsigMatAlum(props) {
     const [selectedSubject1, setSelectedSubject1] = useState("default");
     const [selectedSubject2, setSelectedSubject2] = useState("default");
     const [selectedSubject3, setSelectedSubject3] = useState("default");
-    const materiasPorAño = {
-    '1°': [
-        { label: 'Biología', value: 'Biología' },
-        { label: 'Construccion de la Ciudadanía', value: 'Construccion de la Ciudadanía' },
-        { label: 'Dibujo Técnico I', value: 'Dibujo Técnico I' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'ESI I', value: 'ESI I' },
-        { label: 'Filosofía I', value: 'Filosofía I' },
-        { label: 'Fisicoquímica', value: 'Fisicoquímica' },
-        { label: 'Geografía I', value: 'Geografía I' },
-        { label: 'Historia I', value: 'Historia I' },
-        { label: 'Informatica I', value: 'Informatica I' },
-        { label: 'Ingles I', value: 'Ingles I' },
-        { label: 'Lengua I', value: 'Lengua I' },
-        { label: 'Literatura I', value: 'Literatura I' },
-        { label: 'Matemática I', value: 'Matemática I' },
-        { label: 'Música', value: 'Música' },
-        { label: 'Taller', value: 'Taller' },
-        { label: 'Teatro', value: 'Teatro' }
-    ],
-    '2°': [
-        { label: 'Biología e Higiene', value: 'Biología e Higiene' },
-        { label: 'Dibujo Técnico II', value: 'Dibujo Técnico II' },
-        { label: 'Economía II', value: 'Economía II' },
-        { label: 'Educación Cívica', value: 'Educación Cívica' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'ESI II', value: 'ESI II' },
-        { label: 'Filosofía II', value: 'Filosofía II' },
-        { label: 'Física II', value: 'Física II' },
-        { label: 'Geografía II', value: 'Geografía II' },
-        { label: 'Historia II', value: 'Historia II' },
-        { label: 'Informática II', value: 'Informática II' },
-        { label: 'Inglés II', value: 'Inglés II' },
-        { label: 'Lengua II', value: 'Lengua II' },
-        { label: 'Literatura II', value: 'Literatura II' },
-        { label: 'Matemática II', value: 'Matemática II' },
-        { label: 'Taller', value: 'Taller' },
-        { label: 'Teatro II', value: 'Teatro II' }
-    ],
-    '3°': [
-        { label: 'Dibujo Tecnico III', value: 'Dibujo Tecnico III' },
-        { label: 'Educación Cívica II', value: 'Educación Cívica II' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'Física III', value: 'Física III' },
-        { label: 'Geografía III', value: 'Geografía III' },
-        { label: 'Historia III', value: 'Historia III' },
-        { label: 'Inglés III', value: 'Inglés III' },
-        { label: 'Lengua y Literatura', value: 'Lengua y Literatura' },
-        { label: 'Matemática III', value: 'Matemática III' },
-        { label: 'Química', value: 'Química' },
-        { label: 'Taller', value: 'Taller' }
-    ],
-    '4°': [
-        { label: 'Análisis Matemático', value: 'Análisis Matemático' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'Física IV', value: 'Física IV' },
-        { label: 'Inglés Técnico I', value: 'Inglés Técnico I' },
-        { label: 'Instruccion Civica', value: 'Instruccion Civica' },
-        { label: 'Introducción a la Base de Datos', value: 'Introducción a la Base de Datos' },
-        { label: 'Introducción a la Programación', value: 'Introducción a la Programación' },
-        { label: 'Literatura IV', value: 'Literatura IV' },
-        { label: 'Lógica', value: 'Lógica' },
-        { label: 'Química Aplicada', value: 'Química Aplicada' },
-        { label: 'Taller ', value: 'Taller ' },
-    ],
-    '5°': [
-        { label: 'Ciencia - Tecnologia e Informacion', value: 'Ciencia - Tecnologia e Informacion' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'Estadística y Probabilidad', value: 'Estadística y Probabilidad' },
-        { label: 'Inglés Técnico II', value: 'Inglés Técnico II' },
-        { label: 'Organización y Arquitectura I', value: 'Organización y Arquitectura I' },
-        { label: 'Practicas Profesionalizantes', value: 'Practicas Profesionalizantes' }, //PREGUNTAR EL NOMBRE CORRECTO
-        { label: 'Principios de Testing', value: 'Principios de Testing' },
-        { label: 'Programación Web Estática y Laboratorio Web', value: 'Programación Web Estática y Laboratorio Web' },
-        { label: 'Sistemas Operativos I', value: 'Sistemas Operativos I' },
-        { label: 'Taller', value: 'Taller' },
-        { label: 'Técnicas Avanzadas de Programación', value: 'Técnicas Avanzadas de Programación' },
-        { label: 'Tecnología de Redes I', value: 'Tecnología de Redes I' },
-    ],
-    '6°': [
-        { label: 'Computación Gráfica', value: 'Computación Gráfica' },
-        { label: 'Educación Física', value: 'Educación Física' },
-        { label: 'Etica y Deontologia Profesional', value: 'Etica y Deontologia Profesional' }, //Agregar Practicas profecionalizantes
-        { label: 'Ingles Tecnico III', value: 'Ingles Tecnico III' },
-        { label: 'Introducción a la Automatización y Control', value: 'Introducción a la Automatización y Control' },
-        { label: 'Organización y Arquitectura II', value: 'Organización y Arquitectura II' },
-        { label: 'Programación Web Dinámica', value: 'Programación Web Dinámica' },
-        { label: 'Seguridad Informática', value: 'Seguridad Informática' },
-        { label: 'Sistemas de Gestión de Calidad de Software', value: 'Sistemas de Gestión de Calidad de Software' },
-        { label: 'Sistemas Operativos II', value: 'Sistemas Operativos II' },
-        { label: 'Taller', value: 'Taller' },
-        { label: 'Tecnología de Redes II', value: 'Tecnología de Redes II' }
-    ]
-    };
 
-    const añosPorMateria = {
-        '1': ['1°'],
-        '2': ['1°', '2°'],
-        '3': ['1°', '2°', '3°'],
-    };
     const asignarMateria = () => {
         if (numberOfSubjects === "default") {
             alert("Por favor, seleccione la cantidad de materias");
@@ -142,19 +43,6 @@ export default function AsigMatAlum(props) {
             return;
         }
 
-    console.log(`Asignando ${numberOfSubjects} materia(s): ${selectedSubject} de ${selectedYear} año`);    
-
-    setNumberOfSubjects("default");
-    setSelectedYear("default");
-    setSelectedSubject("default");
-    setNumberOfSubjects("default");
-        setSelectedYear1("default");
-        setSelectedYear2("default");
-        setSelectedYear3("default");
-        setSelectedSubject1("default");
-        setSelectedSubject2("default");
-        setSelectedSubject3("default");
-    alert("Materia(s) asignada(s) con éxito");
     };
     const style = StyleSheet.create({
     container: {
@@ -223,15 +111,15 @@ export default function AsigMatAlum(props) {
             resizeMode={'cover'}
             style={style.backgroundImage}>                
             <View style={style.form}>
-                <Text style={style.modificarAlumno}>Asignar MATERIAS</Text>
+                <Text style={style.modificarAlumno}>Asignar Materia</Text>
     
-                <Text style={{ fontSize: 15 }}>Cuantas materias va a rendir</Text>
+                <Text style={{ fontSize: 15 }}>Cantidad de materias</Text>
                 <Picker
                     selectedValue={numberOfSubjects}
                     onValueChange={(itemValue) => {
                         setNumberOfSubjects(itemValue);
-                        setSelectedYear("default"); // Resetear año al cambiar la cantidad de materias
-                        setSelectedSubject("default"); // Resetear materia al cambiar la cantidad de materias
+                        setSelectedYear("default"); 
+                        setSelectedSubject("default");
                     }}
                     style={style.cajaIng}>
                     <Picker.Item label="Seleccione las materias" value="default" />
